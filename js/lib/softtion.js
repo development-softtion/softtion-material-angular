f486abf9fdb82887cf1c3bd54ecb5d7ac643f3bf
ing-dacastillop <ing.dacastillop@gmail.com>
2017-10-10 08:44:25 -0500

    1. Ajustes en los elementos de Texto 2. Se establecio componente Calendar como un plugin del Framework

--- js/lib/softtion.js
+++ js/lib/softtion.js
@@ -822,4 +822,45 @@
             return new HtmlElement(tag, closed);
         };
     })(window.softtion);
+    
+    (function () {
+        
+        function decimalAdjust(type, value, exp) {
+            // Si el exp no está definido o es cero...
+            if (typeof exp === 'undefined' || +exp === 0) {
+                return Math[type](value);
+            }
+            value = +value;
+            exp = +exp;
+            // Si el valor no es un número o el exp no es un entero...
+            if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+                return NaN;
+            }
+            // Shift
+            value = value.toString().split('e');
+            value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+            // Shift back
+            value = value.toString().split('e');
+            return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+        }
+
+        // Decimal round
+        if (!Math.roundDecimal) {
+            Math.roundDecimal = function (value, exp) {
+                return decimalAdjust('round', value, exp);
+            };
+        }
+        // Decimal floor
+        if (!Math.floorDecimal) {
+            Math.floorDecimal = function (value, exp) {
+                return decimalAdjust('floor', value, exp);
+            };
+        }
+        // Decimal ceil
+        if (!Math.ceilDecimal) {
+            Math.ceilDecimal = function (value, exp) {
+                return decimalAdjust('ceil', value, exp);
+            };
+        }
+    })();
 });
